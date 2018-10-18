6e4b76bcfae3f331765eb236f551ceee92c123d8
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-10-18 17:03:53 -0400

    Update window size display for map, setup, initilize & display map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,68 @@
 import React from "react";
-// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 /*
 Sources:  
   1. https://developers.google.com/maps/documentation/javascript/tutorial
+
   2. https://www.youtube.com/watch?v=W5LhLZqj76s&index=2&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1
 */
 class MyMap extends React.Component {
-  
-  //Initializes Google Map
+  //Google Map setup with parameters 
   initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
-    });
+    });  
+  }
+
+  //First checks to determine if the map has loaded before calling for it to be displayed
+  componentDidMount(){
+    this.displayMap();
+  }
+  
+  //Calls initJScript defined at bottom of file
+  displayMap = () => {
+    initJScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyASu5vRNNzFx1JFJz7SVAIJJoRH9VJcST4&callback=initMap");
+    
+    //binds this map
+    window.initMap = this.initMap;
   }
   
   render(){
     return (
-      <div id="map">Hello</div>
+      <div id="map">
+        Hello
+      </div>
     )
   }
 }
 
-{/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-    async defer>
-</script> */}
+/* Sources for initJScript function https://www.klaasnotfound.com/2016/11/06/making-google-maps-work-with-react/
+
+https://www.youtube.com/watch?v=W5LhLZqj76s&index=2&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1
+
+
+function initJScript will:
+1. obtain the first script tag in the index.html
+2. create another new script tag
+3. get the URL passed to the newly created script tag
+4. insert the newly created script tag before the existing script tag.
+*/
+
+function initJScript(srcURL){
+  const initialScript = window.document.getElementsByTagName("script")[0];
+
+  const newScript = window.document.createElement("script");
+  newScript.src = srcURL;
+  newScript.asyn = true;
+  newScript.defer = true;
+  initialScript.parentNode.insertBefore(newScript, initialScript)
+}
+
 
 /*
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
   React Google Map Source: https://tomchentw.github.io/react-google-maps/
   React Google Map Tutorial: https://www.youtube.com/watch?v=Q0vzqlnWWZw&index=2&list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP
 
