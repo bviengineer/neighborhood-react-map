04b947497de3fa7685dc270c93e54c11a183db00
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-10-28 14:12:58 -0400

    Create a side bar component and include it in the main application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import MyMap from  "./components/Map.js";
+import Listings from "./components/SideMenu.js";
 // import FourSqureAPI from "./api/index.js";
 import './App.css'; 
 
@@ -8,7 +9,8 @@ class App extends React.Component{
   render(){
     return(
       <div>
-        <h1>Local Libraries</h1>
+        <h1 className="listings-title">Local Listings</h1>
+        <Listings />
         <MyMap />
       </div>
     )
---
+++ src/components/SideMenu.js
@@ -0,0 +1,16 @@
+//imports
+import React from "react";
+
+
+//Sidebar with returned locations
+class Listings extends React.Component {
+  render(){
+    return(
+      <div className="side-bar">
+        <p>Search Results will go here</p>
+      </div>
+    )
+  }
+}
+
+export default Listings;
