57b3194c06b23f04435551e61b471a04cf9d412e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-10-30 16:06:33 -0400

    Modify Locations into a Stateless Functional Component

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,5 +1,12 @@
 import React from "react";
+// import { MyMap, venuList } from "./Map.js";
 
+//Stateless Functional Component is responsible for rendering the list of venues
 export const Locations = (props) => {
-  console.log(props.locations)
-}
+    return <div className="venu-list">
+      {props.title}
+      <ul>
+        <li>Test</li>
+      </ul>
+    </div>
+  }
