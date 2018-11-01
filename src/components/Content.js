f419aa93e609ab7dcb65f6a5d11f2706e2b4d579
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-01 13:20:25 -0400

    Fetch data from FourSquare API

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,22 +7,44 @@ import FourSqureAPI from "../api/index.js";
 
 //VARIABLES
 const locationsRequest = "https://api.foursquare.com/v2/venues/explore?"; //End Point Variable
+const searchParams = {
+  client_id: "0TL2LRJP0WLJYNYCVGZKZ4L1YRG50I5CLNP2XG0DUVSUEB2O",
+  client_secret: "4Q21F123T1T05GXLBP1TDEHPTPNLFBJKK341OGNFERSIGYDY",
+  query: "library",
+  near: "Lawrenceville, GA",
+  // limit: 50,  
+  v: "20181015"    
+};
 
 
 //Rendering in App.js
 export class Content extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      locations: []
-    }
+    this.state = { locations: [] }
+    //this.componentDidMount = this.componentDidMount.bind(this)
   }
 
-
+  //Method will call API after map renders
   componentDidMount(){
+    //Request to fourSquare API to retrieve data, using Axios
+    axios.get(locationsRequest + new URLSearchParams(searchParams))
+      .then( response => {        
+        console.log(response); //testing this is the returned data from the API fetch
+        // console.log(response.data.response.groups[0].items[0].venue.name); //for testing        
+        this.setState({ 
+          locations: response.data.response.groups[0].items
+        })         
+            //console.log("data inside of state",  this.state.locations);
+      }) //closing curly bracket & brace for function block and then, respectively
+      .catch(function(err){
+        console.log(err);
+      }) //closing curly bracket & brace for function block and catch, respectively
+    }
 
-
-  }
+  //Parameters for fetching locations from API
+  // fetchLocations = () => {
+  // }
   
   render(){
     return (
