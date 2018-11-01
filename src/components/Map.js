8ef1b480ff49f4b1f2eca3646f8572501791bc06
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-01 14:18:52 -0400

    Receive props from Content component

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,8 +2,9 @@ import React from "react";
 
 
 //Rendering in Content.js
-export class Map extends React.Component{
+export class Map extends React.Component{  
   render(){
+    console.log("console log inside map component render ", this.props.locations); //verifying props is being passed to map from content
     return (
       <div id="map" className="map-loading">{/* ?how's is connected to getElementById from map listed in App.js */}
           <h1>{this.props.note}</h1> {/* Prints a message to the screen that the map is loading */}
@@ -24,71 +25,4 @@ Sources:
   7. Tutorial https://www.youtube.com/watch?v=W5LhLZqj76s&index=2&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1
   
   https://youtu.be/hzLDsxPGctY (fetch tutorial) - DID NOT USE
-
-
-
-//IMPORTS
-import React from "react";
-// import axios from "axios"; //will handle request to API
-import { Locations } from "./Locations.js";
-
-//VARIABLES
-// const locationsRequest = "https://api.foursquare.com/v2/venues/explore?"; //End Point Variable
-
-
-//Map Component Class
-export class Map extends React.Component {
-
-  componentDidUpdate(){
-    // this.addMapMarkers(this.props.locations);
-    // console.log("test from cdidupdate ", this.props.locations)
-  }
-  //Will loop through the array of destinations returned from the fetch request   
-  addMapMarkers = (locations) => {
-    console.log("window.google ", window.google)
-   if(window.google){
-      console.log("from addMapMarkers ", window.map)
-      locations.map(destination => {
-            
-        //Infowindow variable that will display content on the map marker for a given destination
-        const infoWindowData = `<strong>${destination.venue.name}</strong> <br>
-          ${destination.venue.location.address} <br>
-          ${destination.venue.location.formattedAddress[1]} <br>
-          ${destination.venue.location.country}`
-        ;
-      
-          //Creats a map marker for each destnation in the appray and & adds them to the map
-        let marker = new window.google.maps.Marker({
-          position: {lat: destination.venue.location.lat, lng: destination.venue.location.lng},  
-          map: window.map,
-          title: destination.venue.name
-        });
-        
-         //Event listener for each map marker that will pop up an infowindow
-         marker.addListener('click', function(){
-          window.infowindow.setContent(infoWindowData);
-          window.infowindow.open(window.map, marker);
-        });
-      }); //closing curl brace & bracket for this.state.loctions.map
-    }    
-  }  
-  
-    render(){
-      this.addMapMarkers(this.props.locations);
-      console.log("test from cdidupdate ", this.props.locations)
-    return <div id="map"></div>
-  }   
-}
-
-
-
-/* Sources for initJScript function:
-1. https://www.klaasnotfound.com/2016/11/06/making-google-maps-work-with-react/
-2. https://www.youtube.com/watch?v=W5LhLZqj76s&index=2&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1
-
-function initJScript will:
-1. obtain the first script tag in the index.html
-2. create a new script tag
-3. get the URL passed to the newly created script tag
-4. insert the newly created script tag before the existing script tag.
 */
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,6 +5,7 @@ import { Search } from "./Search.js"
 //Rendering in Content.js
 export class SideMenu extends React.Component {
   render(){
+    console.log("console log inside SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <Search userInput="User search term here"/>
@@ -12,3 +13,7 @@ export class SideMenu extends React.Component {
     );
   }
 }
+
+/*
+1. render returned results of search and display them here in the side bar
+*/
