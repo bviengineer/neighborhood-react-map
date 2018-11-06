ced0df5d29253e01a1f94ae2f95a73fc8cc644fa
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-06 13:33:27 -0500

    Rendering data inside SideMenu.js

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,14 +2,24 @@ import React from "react";
 import { Search } from "./Search.js";
 import "../App.css";
 
-
 //Rendering in Content.js
-export class SideMenu extends React.Component {
+export class SideMenu extends React.Component {  
+  
   render(){
     console.log("console log inside SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <Search userInput="User search term here"/>
+        <div>
+          <ol>
+          {this.props.locations.map(destination => (
+            <li>
+              {destination.name}
+            </li>
+          ))
+          }
+          </ol>
+        </div>
       </div>
     );
   }
@@ -17,4 +27,6 @@ export class SideMenu extends React.Component {
 
 /*
 1. render returned results of search and display them here in the side bar
+
+
 */
