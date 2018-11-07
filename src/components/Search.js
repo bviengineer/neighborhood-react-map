99d4da227c457b82860a2dcabb24e5d9aa0ab05a
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-07 10:38:15 -0500

    Event handler for user input in search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in SideMenu.js
 export const Search = (props) => {
-  return <input className="search-bar" placeholder={props.userInput} />
+  return <input className="search-bar" placeholder={props.userInput} onKeyPress={(e) => props.onKeyPress(e.target.value)}/>
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,15 +2,21 @@ import React from "react";
 import { Search } from "./Search.js";
 import "../App.css";
 
+
 //Rendering in Content.js
-export class SideMenu extends React.Component {  
+export class SideMenu extends React.Component { 
+  
+  //
+  handleSearch = (e) => {
+    console.log(e)
+  }
   
   render(){
     console.log("console log inside SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <h4>Venu Results</h4>
-        <Search userInput="search venues"/>
+        <Search userInput="search venues" onKeyPress={this.handleSearch}/>  {/* Search Component */}
         <div>
           <ol>
           {this.props.locations.map(destination => (
