91859c4cad4aae000b93f0d7ca0c9d3e7d2057a0
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-07 12:19:11 -0500

    Attributes on new line for easier read

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,5 +3,11 @@ import React from "react";
 
 //Rendering in SideMenu.js
 export const Search = (props) => {
-  return <input className="search-bar" placeholder={props.userInput} onChange={(e) => props.onKeyPress(e.target.value)}/>
+  return( <input 
+            className="search-bar" 
+            placeholder={props.userInput} 
+            onChange={(e) => props.onChange(e.target.value)}
+            onKeyPress={props.filterSearch}
+          />
+  )
 }
