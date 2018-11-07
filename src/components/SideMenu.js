31b5381758d05e62e655c38041dcb79aa3d2a90b
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-07 11:12:35 -0500

    Add state to hold user query

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,14 +5,31 @@ import "../App.css";
 
 //Rendering in Content.js
 export class SideMenu extends React.Component { 
-  
-  //
+  constructor(props){ 
+    super(props);
+    this.state = {
+      userQuery: this.props.locations
+    }
+  }
+
+
+  //Gets keywords typed by user
   handleSearch = (e) => {
     console.log(e)
   }
+
+  componentDidMount(){
+    this.filterSearch();
+  }
+
+  //Filter user search results
+  filterSearch = (query) => {
+    this.setState({ userQuery: query });
+  }
   
   render(){
-    console.log("console log inside SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
+    console.log("SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
+    console.log("STATE inside SideMenu", this.props.userQuery);
     return (
       <div className="side-bar">
         <h4>Venu Results</h4>
