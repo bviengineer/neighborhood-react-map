44327507ad1657094d19216fac7111a13c1741ed
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 18:58:16 -0500

    Add aria role and tabindex

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in App.js
 export const Footer = (props) => {
-  return <h1>{props.heading}</h1>
+  return <h1 role="Contentinfo" tabIndex="0">{props.heading}</h1>
 }
