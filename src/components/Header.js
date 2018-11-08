ba7393dd58dfcb91da6a5f7c4e0bf5331fbb7998
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 17:51:14 -0500

    Add aria role and tab index

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in App.js
 export const Header = (props) => {
-  return <h1>{props.heading}</h1>
+  return <h1 role="heading" tabindex="0">{props.heading}</h1>
 }
