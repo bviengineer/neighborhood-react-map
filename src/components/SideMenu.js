4583f14d3c359d51626d65b72663c818db009965
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 12:17:03 -0500

    Move search feature into SideMenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Search } from "./Search.js";
+//import { Search } from "./Search.js";
 import "../App.css";
 
 
@@ -8,34 +8,49 @@ export class SideMenu extends React.Component {
   constructor(props){ 
     super(props);
     this.state = {
-      userQuery: ""
+      userQuery: []
     }
   }
 
-
+ 
   //Gets keywords typed by user
   handleSearch = (e) => {
     console.log(e)
+    this.setState({userQuery: this.props.locations});
+    //set the state to e.target.value and then filter props based on state?
+    this.state.userQuery.filter(this.onChange);
   }
 
-  //Filter user search results
-  filterSearch = () => {
-    this.setState({ userQuery: this.handleSearch });
-    console.log("printing from filter search");
+  //Event handler
+  onChange = (e) => {
+    this.handleSearch(e.target.value)
   }
+ //const popular = musicData.filter(artist => artist.sales > 1000000).map(artist => {return artist.artist + " is a great performer"});
+
+  //NOTE TO SELF: filter the the state passing in the userQuery as the parameter
+  //Filter user search results
+  // filterSearch = () => {
+  //   this.state.userQuery.filter(venue => {
+  //     this.handleSearch(e.target.value)
+  //   }) 
   
   render(){
-    console.log("SideMenu component render ", this.props.locations); //verifying props is being passed to SideMenu from content
+    console.log("SideMenu component render after setting state ", this.state.userQuery); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <h4>Venu Results</h4>
 
+       <input 
+            className="search-bar" 
+            placeholder={this.props.userInput} 
+            onChange={this.onChange}
+          />
         {/* Search Component */}
-        <Search 
+        {/* <Search 
           userInput="search venues" 
           onChange={this.handleSearch} 
           onKeyPress={this.filterSearch}
-        />  
+        />   */}
         
         <div>
           <ol>
