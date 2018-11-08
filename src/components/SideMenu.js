682ddefdd97049b027ee06f81e8bec9717c80216
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 15:55:05 -0500

    Develop query feature

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -15,12 +15,22 @@ export class SideMenu extends React.Component {
   //Gets keywords typed by user
   handleSearch = (e) => {
     let queryString = e.target.value;
-    console.log("queryString data ", queryString)
-    // this.setState({userQuery: this.props.locations});
+
+    console.log("state BEFORE setState ", this.state.userQuery)
     
-    // if(this.state.userQuery != ""){
-    //   this.props.filterSearch();
-    // }
+    if(this.state.userQuery == ""){
+      this.setState({ 
+        userQuery: queryString
+      })
+      //testing query and state before query change
+      console.log("state AFTER setState ", this.state.userQuery)
+    } else if(this.state.userQuery != "" && this.state.userQuery != queryString){
+      this.setState({
+        userQuery: queryString
+      })
+      console.log("new query ", this.state.userQuery)
+    }
+
   }
 
  //const popular = musicData.filter(artist => artist.sales > 1000000).map(artist => {return artist.artist + " is a great performer"});
@@ -33,7 +43,7 @@ export class SideMenu extends React.Component {
 */
   
   render(){
-    console.log("SideMenu component render after setting state ", this.state.userQuery); //verifying props is being passed to SideMenu from content
+    //console.log("SideMenu component render after setting state ", this.state.userQuery); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <h4>Venu Results</h4>
