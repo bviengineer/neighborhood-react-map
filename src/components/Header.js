d54e934d17e71728aa27742eba0ddcc5bc55e3b5
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 20:38:58 -0500

    Remove aria role of heading - role was redundant

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in App.js
 export const Header = (props) => {
-  return <h1 role="heading" tabIndex="0">{props.heading}</h1>
+  return <h1 tabIndex="0">{props.heading}</h1>
 }
