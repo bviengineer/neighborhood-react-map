3b7e6e5de970f65bf8617901c4aa2e96597ca433
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 20:14:40 -0500

    Remove aria role and tabindex and replaed h1 with semantic tag: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in App.js
 export const Footer = (props) => {
-  return <h1 role="Contentinfo" tabIndex="0">{props.heading}</h1>
+  return <footer>>{props.heading}</footer>
 }
