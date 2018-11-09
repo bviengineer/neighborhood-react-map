b585ecad5fc8b1349b844eeb170873571e6786f5
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-08 20:39:34 -0500

    Correct spelling of aria-label

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,7 @@ export class Map extends React.Component{
   render(){
     // console.log("console log inside map component render ", this.props.locations); //verifying props is being passed to map from content
     return (
-      <div id="map" className="map-loading" role="application" aria-lablel="map" tabIndex="-1">{/* ?how's is connected to getElementById from map listed in App.js */}
+      <div id="map" className="map-loading" role="application" aria-label="map" tabIndex="-1">{/* ?how's is connected to getElementById from map listed in App.js */}
           <h1>{this.props.note}</h1> {/* Prints a message to the screen that the map is loading */}
       </div> 
     );
