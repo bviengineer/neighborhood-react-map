bb3f3aca8dab16a4dfed9b0a572da4e044b47267
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-10 18:33:11 -0500

    Remove props and set content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 
 //Rendering in App.js
-export const Footer = (props) => {
-  return <footer>{props.heading}</footer>
+export const Footer = () => {
+  return <footer> View Project on <a href="https://github.com/bviengineer/neighborhood-react-map" target="_blank">GitHub</a></footer>
 }
