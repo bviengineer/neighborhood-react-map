ce8f60a14929a81dd810db1aed6c70d697ebb48f
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-09 22:45:32 -0500

    Filter feature

--- src/components/Content.js
+++ src/components/Content.js
@@ -50,7 +50,7 @@ export class Content extends React.Component {
     this.state.locations.map(destination => {              
       //Infowindow variable that will display content on the map marker for a given destination
       const infoWindowData = `<span role="link" tabIndex="0"><strong>${destination.venue.name}</strong></span> <br>
-        ${destination.venue.location.address}<<br>
+        ${destination.venue.location.address}<br>
         ${destination.venue.location.formattedAddress[1]}<br>
         ${destination.venue.location.country}`
       ;
@@ -88,10 +88,11 @@ export class Content extends React.Component {
           locations:  location
         })
       })
-    } else {
-    this.setState({
-      locations: this.state.locations
-    })
+  //   } else {
+  //   this.setState({
+  //     locations: this.state.locations
+  //   })
+  // }
   }
 }
 
