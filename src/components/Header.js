fea4ce6c11a6c4bffb68aa2080b3fbd4ecb70fcb
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-10 22:13:58 -0500

    Add an h1 tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,5 +3,9 @@ import React from "react";
 
 //Rendering in App.js
 export const Header = () => {
-  return <header tabIndex="0">Neighbor<span>hood</span> Libraries</header>
+  return( 
+    <header tabIndex="0">
+      <h1>Neighbor<span>hood</span> Libraries</h1>
+    </header> 
+  )
 }
