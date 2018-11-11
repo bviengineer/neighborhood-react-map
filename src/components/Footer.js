67a8047f204334454315890dffcdbea7b342543e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-10 20:26:45 -0500

    Update styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,5 @@ import React from "react";
 
 //Rendering in App.js
 export const Footer = () => {
-  return <footer> View Project on <a href="https://github.com/bviengineer/neighborhood-react-map" target="_blank">GitHub</a></footer>
+  return <footer> View <span>Project</span> repo on <a href="https://github.com/bviengineer/neighborhood-react-map" target="_blank">GitHub</a></footer>
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,6 @@ import React from "react";
 
 
 //Rendering in App.js
-export const Header = (props) => {
-  return <header tabIndex="0">{props.heading}</header>
+export const Header = () => {
+  return <header tabIndex="0">Neighbor<span>hood</span> Libraries</header>
 }
