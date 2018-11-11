3358344610694c39d50155ed9d078182252405c2
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-11 18:00:43 -0500

    Setup an event listener for each menu item

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,15 +5,25 @@ import "../App.css";
 
 //Rendering in Content.js
 export class SideMenu extends React.Component {   
+  // handleClick = (e) => {
+  //   e.target.addEventListener("click", function(){
+  //     console.log("your clicked on ", this.props.locations);
+  //   });
+  // }
+  
   render(){
-    //console.log("SideMenu component render after setting state ", this.state.userQuery); //verifying props is being passed to SideMenu from content
     return (
       <div className="side-bar">
         <h4>Libraries</h4>        
         <div>
           <ol role="list" tabIndex="0">
           {this.props.locations.map(destination => (
-            <li role="link" tabIndex="0" key={destination.referralId}>
+            <li 
+              role="link" 
+              tabIndex="0" 
+              key={destination.referralId}
+              onClick={(e) => { e.target.addEventListener("click", function(){ console.log("you clicked on ", destination.venue.name)}) }}
+              >
               <strong>{destination.venue.name}</strong>
               <br/>
               {destination.venue.location.formattedAddress[0]}<br/>
