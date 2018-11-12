30b11a735371216cb2caea5a24e49f27aec49b7d
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-12 11:38:15 -0500

    Implement google map failure error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { ErrorBoundary } from "./components/errorBoundary.js";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { App } from './App.js';
+
+
+//Working concept for google authentication concept found here: https://github.com/MatthewCranford/neighborhood-map/blob/master/src/index.js
+window.gm_authFailure = () => {
+  ReactDOM.render(
+    <h3>Google map fail to properly load, please try again.</h3>, 
+    window.document.getElementById('map'));
+ }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
